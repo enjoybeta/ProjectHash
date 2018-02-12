543ea736b550965b11617b717146efee740840ce
enjoybeta <goujiashun@gmail.com>
2018-02-12 18:34:27 -0500

    v0.2.3
    import picasso for photo displaying

--- mobile_app/app/src/main/java/hash/application/fragments/Fragment1.kt
+++ mobile_app/app/src/main/java/hash/application/fragments/Fragment1.kt
@@ -5,12 +5,23 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.squareup.picasso.Picasso
 import hash.application.R
+import kotlinx.android.synthetic.main.fragment1.*
 
 class Fragment1: Fragment() {
-
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater!!.inflate(R.layout.fragment1, container, false)
+    }
+
+    override fun onStart() {
+        retrivePhoto()
+        super.onStart()
+    }
+
+    fun retrivePhoto() {
+        Picasso.with(activity).load("https://cdn.shopify.com/s/files/1/1190/4748/t/10/assets/logo.png").into(imageView3);
+        Picasso.with(activity).load(R.drawable.image_item4).into(imageView4);
     }
 
 }
