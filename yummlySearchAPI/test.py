0fbd79f821c42a29c5e0edb253d8b60817e6ce5c
Harry <xuj10@rpi.edu>
2018-02-20 01:09:35 -0500

    Add yummly api test files

---
+++ yummlySearchAPI/test.py
@@ -0,0 +1,25 @@
+import urllib.request, urllib.parse, json
+
+class YummlySearch:
+    def __init__(self, baseSearchAddress_ = 'http://api.yummly.com/v1/api/recipes?', appId_ = '4a4d16bf', appKey_ = 'c60f961fefede4811a9f5e9709cb6829'):
+        self.baseSearchAddress = baseSearchAddress_
+        self.appId = appId_
+        self.appKey = appKey_
+
+    def generateSearchRequest(self, keyWords):
+        if (not keyWords):
+            raise RuntimeError('Key words cannot be empty')
+        queryWord = keyWords[0]
+        for i in range(1, len(keyWords)):
+            queryWord += '+' + keyWords[i]
+        parameter = {'_app_id':self.appId, '_app_key':self.appKey, 'q': queryWord}
+        addressParameter = urllib.parse.urlencode(parameter)
+        return self.baseSearchAddress + addressParameter
+
+    def getSearchResult(self, searchRequest, requestTimeout = 10):
+        return json.loads(urllib.request.urlopen(searchRequest, timeout = requestTimeout).read())
+
+if __name__ == '__main__':
+    search = YummlySearch()
+    searchRequest = search.generateSearchRequest(['onion', 'soup'])
+    print(json.dumps(search.getSearchResult(searchRequest), indent=4))
