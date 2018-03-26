5a518e41e6a0dcd22b29c00535ba03326eaf7761
RioMichael <lix23@rpi.edu>
2018-03-26 17:46:24 -0400

    add comment

--- Database/database_to_json.py
+++ Database/database_to_json.py
@@ -4,18 +4,21 @@ import cassandra
 from cassandra.cluster import Cluster
 from pprint import pprint
 
-
+'''sample python program to simulate situation that return json file based on a search in database'''
 if __name__ == '__main__':
+	#create connection to database
 	cluster = Cluster()
 	session = cluster.connect('hash')
+
+	#specific a recipe to search in database
 	id = "French-Onion-Soup-2141595"
 	rows = session.execute('''
 				SELECT * from public_recipe where id = %s
 				''',
 				(id,)
 			)
+	#format the result data from database
 	for row in rows:
-		#return_data = row
 		return_data = { 'name': row.name,
 						'id': row.id,
 						'ingredientLines': row.ingredients,
@@ -24,7 +27,8 @@ if __name__ == '__main__':
 						'imageURLs': row.imageurl,
 						'flavor': row.flavor,
 						'instructionurl': row.instruction}
+		#write the data into a json file
 		with open('return2.json', 'w') as outfile:
 			json.dump(return_data, outfile)
-		#print return_data
+	#close database connection
 	cluster.shutdown()
