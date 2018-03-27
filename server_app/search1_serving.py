a89d06a5256fa9d06811d7d7587e6331fc1db0c7
RioMichael <lix23@rpi.edu>
2018-03-27 18:40:37 -0400

    search functions
    search1_serving: search based on number of serving
    search2_keyword: search based on keyword in the recipe name

---
+++ server_app/search1_serving.py
@@ -0,0 +1,42 @@
+import sys
+import json
+import cassandra
+from cassandra.cluster import Cluster
+from pprint import pprint
+
+'''sample python function to simulate situation that return json file based on a search in database'''
+def search_serving(request_json):
+	#create connection to database
+	cluster = Cluster()
+	session = cluster.connect('hash')
+	data = json.load(open(request_json))
+	#specific a recipe to search in database
+	#id = "French-Onion-Soup-2141595"
+	num = data["numberofserving"]
+	rows = session.execute('''
+				SELECT * from public_recipe where numberofserving = %s allow filtering
+				''',
+				(num,)
+			)
+	#format the result data from database
+	return_list = []
+	for row in rows:
+		return_data = { 'name': row.name,
+						'id': row.id,
+						'ingredientLines': row.ingredients,
+						'totaltime': row.time,
+						'numberofserving': row.numberofserving,
+						'imageURLs': row.imageurl,
+						'flavor': row.flavor,
+						'instructionurl': row.instruction}
+		#write the data into a json file
+		#name = row.id + '.json'
+		return_list.append(return_data)
+	#with open("search1_return.json", 'w') as outfile:
+	json_return = json.dumps(return_list)
+	return json_return
+	#close database connection
+	cluster.shutdown()
+
+if __name__ == "__main__":
+    search_serving('search1_request.json')
---
+++ server_app/search2_keyword.py
@@ -0,0 +1,42 @@
+import sys
+import json
+import cassandra
+from cassandra.cluster import Cluster
+from pprint import pprint
+
+'''sample python function to simulate situation that return json file based on a search in database'''
+def search_keyword(request_json):
+	#create connection to database
+	cluster = Cluster()
+	session = cluster.connect('hash')
+	data = json.load(open(request_json))
+	#specific a recipe to search in database
+	#id = "French-Onion-Soup-2141595"
+	keyword = data["keyword"]
+	rows = session.execute('''
+				SELECT * from public_recipe where name like '%''' + keyword + '''%' allow filtering
+				'''
+				#(keyword,)
+			)
+	#format the result data from database
+	return_list = []
+	for row in rows:
+		return_data = { 'name': row.name,
+						'id': row.id,
+						'ingredientLines': row.ingredients,
+						'totaltime': row.time,
+						'numberofserving': row.numberofserving,
+						'imageURLs': row.imageurl,
+						'flavor': row.flavor,
+						'instructionurl': row.instruction}
+		#write the data into a json file
+		#name = row.id + '.json'
+		return_list.append(return_data)
+	#with open("search1_return.json", 'w') as outfile:
+	json_return = json.dumps(return_list)
+	return json_return
+	#close database connection
+	cluster.shutdown()
+
+if __name__ == "__main__":
+    search_keyword('search2_request.json')
