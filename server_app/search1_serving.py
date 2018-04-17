fb176f1f6eb9e052d42f4593b5be19075c8d1f1f
RioMichael <lix23@rpi.edu>
2018-04-17 19:14:34 -0400

    add comments

--- server_app/search1_serving.py
+++ server_app/search1_serving.py
@@ -6,7 +6,7 @@ from pprint import pprint
 
 '''
 	python function that return json file based on a numberofserving search in database
-   	@request:  json object of input data
+   	@request:  json object of input data (numofserving, having, not_having)
 '''
 def search_serving(request):
 	#create connection to database
@@ -24,21 +24,25 @@ def search_serving(request):
 				''',
 				(num,)
 			)
-	#format the result data from database
+	#process the result data from database
 	return_buff = []
 	count = 0
 	for row in rows:
 		buff = []
+		#count how many ingredients that the recipe needs are in 'having' list
 		for h_ingre in h_ingredients:
 			for ingredient in row.ingredients:
 				if h_ingre in ingredient:
 					count += 1
 					break
+		#check if any ingredient that the recipe needs is in 'not_having' list
 		for n_ingre in n_ingredients:
 			for ingredient in row.ingredients:
 				if n_ingre in ingredient:
 					count = 0
 					break
+		#any recipe that having at least one ingredient in 'having' list and no ingredient in
+		#'not_having' list would be a potential result recipe
 		if float(count) / len(row.ingredients) > 0.01 :
 			return_data = { 'name': row.name,
 							'id': row.id,
@@ -53,8 +57,10 @@ def search_serving(request):
 		#write the data into a json file
 			return_buff.append(buff)
 		count = 0
+	#sort based on the number of ingredients in the 'having list'
 	return_buff = sorted(return_buff, key = lambda buff: buff[1], reverse = True)
 	return_list = []
+	#return the top 10 recipes as result
 	for i in range(min(10, len(return_buff))):
 		return_list.append(return_buff[i][0])
 	#encode the result data into json object
--- server_app/user_manager.py
+++ server_app/user_manager.py
@@ -3,23 +3,35 @@ import json
 import cassandra
 from cassandra.cluster import Cluster
 
+'''user_manager ccontains four functions that are needed for user profile'''
 
+'''
+	sign_up function takes a json object that contains user's sign up information
+	and puts the information into database and returns success message.
+	If username already exist, return fail message.
+	@request: json object of user's sign up data (username, email, password)
+'''
 def sign_up(request):
 	#create connection to database
 	cluster = Cluster()
 	session = cluster.connect('hash')
+
+	#get user's sign up data
 	data = json.loads(request.decode('utf-8'))
 	username = data["username"]
 	email = data["email"]
 	password = data["password"]
+
+	#check if the username already exists
 	rows = session.execute('''
 				SELECT * from user where username = %s allow filtering
 				''',
 				(username,)
 			)
-	if rows:
+	if rows: 
 		return "Username already exists!"
 	else:
+		#insert user information to database
 		session.execute('''
 				INSERT INTO user (id, username, email, password)
 				VALUES (now(), %s, %s, %s)
@@ -31,13 +43,23 @@ def sign_up(request):
 	cluster.shutdown()
 
 
+'''
+	login function takes a json object that contains user's login information
+	return success message if username exist and passwor is correct. Otherwise,
+	return fail message
+	@request: json object of user's login data (username, password)
+'''
 def login(request):
 	#create connection to database
 	cluster = Cluster()
 	session = cluster.connect('hash')
+
+	#get user's login data
 	data = json.loads(request.decode('utf-8'))
 	username = data["username"]
 	password = data["password"]
+
+	#check if the username exists
 	rows = session.execute('''
 				SELECT * from user where username = %s allow filtering
 				''',
@@ -45,6 +67,7 @@ def login(request):
 			)
 	if rows:
 		for row in rows:
+			#check if the password is correct
 			if password == row.password:	
 				return "Login success!"
 			else:
@@ -56,15 +79,25 @@ def login(request):
 	cluster.shutdown()
 
 
+'''
+	upload function takes a json object that contains user's information that need 
+	to be updated in the database and update the information to the database
+	If username or password is wrong, return fail message.
+	@request: json object of user's data (username, password, favourite, ingredients)
+'''
 def upload(request):
 	#create connection to database
 	cluster = Cluster()
 	session = cluster.connect('hash')
+
+	#get user's upload data
 	data = json.loads(request.decode('utf-8'))
 	username = data["username"]
 	password = data["password"]
 	favourite = data["favourite"]
 	ingredients = data["ingredients"]
+
+	#check if the username exists
 	rows = session.execute('''
 				SELECT * from user where username = %s allow filtering
 				''',
@@ -72,7 +105,9 @@ def upload(request):
 			)
 	if rows:
 		for row in rows:
-			if password == row.password:	
+			#check if the password is correct
+			if password == row.password:
+				#update the data in the database	
 				session.execute('''
 					UPDATE user SET favourite = %s, ingredients = %s WHERE username = %s
 					''',
@@ -88,13 +123,23 @@ def upload(request):
 	cluster.shutdown()
 
 
+'''
+	download function takes a json object that contains user's information
+	and get the data of corresponding user from database and returns the data.
+	If username or password is wrong, return fail message.
+	@request: json object of user's sign up data
+'''
 def download(request):
 	#create connection to database
 	cluster = Cluster()
 	session = cluster.connect('hash')
+
+	#get user's information
 	data = json.loads(request.decode('utf-8'))
 	username = data["username"]
 	password = data["password"]
+
+	#check username
 	rows = session.execute('''
 				SELECT * from user where username = %s allow filtering
 				''',
@@ -102,7 +147,9 @@ def download(request):
 			)
 	if rows:
 		for row in rows:
+			#check password
 			if password == row.password:	
+				#gget data and return
 				return_data = { 'username': row.username,
 							'favourite': row.favourite,
 							'ingredients': row.ingredients}
