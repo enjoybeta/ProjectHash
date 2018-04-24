a0a8713ec1b39a78b6811d0c8d590ceead8b4bbf
enjoybeta <goujiashun@gmail.com>
2018-04-24 18:01:49 -0400

    v1.3.4
    update user suggestion daily

--- server_app/app.py
+++ server_app/app.py
@@ -1,13 +1,19 @@
 from flask import Flask,request
 import json
+from apscheduler.schedulers.background import BackgroundScheduler
 from search1_serving import search_serving
 from search2_keyword import search_keyword
-from user_manager import sign_up
-from user_manager import login
-from user_manager import upload
-from user_manager import download
+from user_manager import sign_up,login,upload,download
+from random_today import random_today
 
 app = Flask(__name__)
+sched = BackgroundScheduler(daemon=True)
+sched.add_job(updateTodaysRecipe,'cron',CronTrigger.from_crontab('0 0 * * *'),jitter=0)
+sched.start()
+today1 = ""
+today2 = ""
+today3 = ""
+today4 = ""
 
 @app.route("/")
 def guidance():
@@ -15,27 +21,19 @@ def guidance():
     
 @app.route("/today1")
 def today1():
-    with open ("../Database/examples/1.json", "r") as myfile:
-        data=myfile.readline()
-    return data
+    return today1
     
 @app.route("/today2")
 def today2():
-    with open ("../Database/examples/2.json", "r") as myfile:
-        data=myfile.readline()
-    return data
+    return today2
     
 @app.route("/today3")
 def today3():
-    with open ("../Database/examples/3.json", "r") as myfile:
-        data=myfile.readline()
-    return data
+    return today3
     
 @app.route("/today4")
 def today4():
-    with open ("../Database/examples/4.json", "r") as myfile:
-        data=myfile.readline()
-    return data
+    return today4
     
 @app.route('/searchPrecise', methods=['POST', 'GET'])
 def search_precise():
@@ -117,6 +115,13 @@ def validate_json(data):
         print("Not valid Json")
         return False
     return True
+
+def updateTodaysRecipe(data):
+    jsonArray = json.loads(random_today())
+    today1 = jsonArray[0]
+    today2 = jsonArray[1]
+    today3 = jsonArray[2]
+    today4 = jsonArray[3]
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1')
